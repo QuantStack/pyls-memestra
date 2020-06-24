5727022df887fba5f1f8c7769922df96235c3d0a
Mariana Meireles <mariana@psychonautgirl.space>
2020-06-24 21:41:40 +0200

    Version 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyls-memestra",
-    version="0.0.2",
+    version="0.0.1",
     description="Memestra plugin for the Python Language Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
