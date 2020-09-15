84a89b6e972c4adedaeefdd5db3cfe223f0a2dfa
Mariana Meireles <mariana@psychonautgirl.space>
2020-09-15 19:09:41 +0200

    Make pylsmemestra compatible with deprecated deprecated

--- pyls_memestra/plugin.py
+++ pyls_memestra/plugin.py
@@ -1,24 +1,37 @@
 from pyls import hookimpl, lsp
 from memestra import memestra
 
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 @hookimpl
 def pyls_lint(document):
     diagnostics = []
     with open(document.path, 'r') as code:
-        deprecated_uses = memestra(code, decorator=("decoratortest", "deprecated"))
+        deprecated_uses = memestra(code, decorator=("deprecated", "deprecated"),
+            reason_keyword="reason")
         diagnostics = format_text(deprecated_uses, diagnostics)
     return diagnostics
-
+ 
 def format_text(deprecated_uses, diagnostics):
-    for fname, fd, lineno, colno in deprecated_uses:
+    for fname, fd, lineno, colno, reason in deprecated_uses:
         err_range = {
             'start': {'line': lineno - 1, 'character': colno},
             'end': {'line': lineno - 1, 'character': len(fname)},
         }
-        diagnostics.append({
-            'source': 'memestra',
-            'range': err_range,
-            'message': fname + " is deprecated.",
-            'severity': lsp.DiagnosticSeverity.Information,
-        })
+        if reason != "reason":
+            diagnostics.append({
+                'source': 'memestra',
+                'range': err_range,
+                'message': fname + " is deprecated. " + reason,
+                'severity': lsp.DiagnosticSeverity.Information,
+            })
+        else:
+            diagnostics.append({
+                'source': 'memestra',
+                'range': err_range,
+                'message': fname + " is deprecated.",
+                'severity': lsp.DiagnosticSeverity.Information,
+            })
     return diagnostics
