4d72527b2bc3572e796b514e8469a5058b571d0b
Mariana Meireles <mariana@psychonautgirl.space>
2020-09-21 23:09:15 +0200

    Version 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Mariana Meireles'
 author = 'Mariana Meireles'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
