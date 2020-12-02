a5b9a762727190ce73920b6a0891e12ac51cacb4
Mariana Meireles <mariana@psychonautgirl.space>
2020-12-02 16:01:53 +0200

    Version 0.0.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Mariana Meireles'
 author = 'Mariana Meireles'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyls-memestra",
-    version="0.0.6",
+    version="0.0.7",
     description="Memestra plugin for the Python Language Server",
     long_description=long_description,
     long_description_content_type="text/markdown",
